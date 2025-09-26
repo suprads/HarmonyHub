ff6418fd06f5ee01f9f53601f10fda69099497d3
DragonGuyMan <108588518+DragonGuyMan@users.noreply.github.com>
2025-09-25 20:46:24 -0400

    Added prettier formatter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslintConfigPrettier,
   {
     ignores: [
       "node_modules/**",
