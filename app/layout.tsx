1825d47f43a28f6cd01f7cf3a285ee38f0522684
Dilpreet Supra <suprads@mail.uc.edu>
2025-10-20 20:48:47 -0400

    Nested Pages Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from 'next/link'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+              <nav>
+                  <Link href="/">Home</Link>
+                  <Link href="/search">Search</Link>
+                  <Link href="/ratings">Ratings</Link>
+                  <Link href="/chart">Chart</Link>
+                  <Link href="/settings">Settings</Link>
+              </nav>
+              {children}
       </body>
     </html>
   );
