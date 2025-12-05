f85c03d72368d99765610a4f04dd55b21bca8a33
Austin Schnee <108588518+DragonGuyMan@users.noreply.github.com>
2025-12-05 12:02:13 -0500

    Set up Prisma to seed database

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   schema: "lib/prisma/schema.prisma",
   migrations: {
     path: "lib/prisma/migrations",
-    // seed: "node lib/prisma/seed.ts",
+    seed: "tsx lib/prisma/seed.ts",
   },
   datasource: {
     url: env.DATABASE_URL ?? "",
