9735f1527a3f42bd3d54230b73368aefbf0dedfc
Austin Schnee <108588518+DragonGuyMan@users.noreply.github.com>
2025-12-06 13:57:19 -0500

    Prisma client loader updated to allow only one client instance

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,19 @@
-import "../envConfig"; // Ensure environment variables are loaded
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 // Optionally, you can log the loaded environment variables for debugging
-//console.log(`The connection URL is ${process.env.DATABASE_URL}`);
+// console.log(`The connection URL is ${connectionString}`);
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
 
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+
+// Global usage meant to ensure only one instance of the client exists
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export { prisma };
