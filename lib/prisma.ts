fb9676376cb5a95bf4589f4b4da547167f88137e
Austin Schnee <108588518+DragonGuyMan@users.noreply.github.com>
2025-12-06 13:43:13 -0500

    Moved prisma folder to root of project

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
 import "../envConfig"; // Ensure environment variables are loaded
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 // Optionally, you can log the loaded environment variables for debugging
--- prisma.config.ts
+++ prisma.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from "prisma/config";
 import { env } from "process";
 
 export default defineConfig({
-  schema: "/lib/prisma/schema.prisma",
+  schema: "prisma/schema.prisma",
   migrations: {
-    path: "lib/prisma/migrations",
-    seed: "tsx lib/prisma/seed.ts",
+    path: "prisma/migrations",
+    seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: env.DATABASE_URL!,
+    url: env.DATABASE_URL ?? "",
   },
 });
