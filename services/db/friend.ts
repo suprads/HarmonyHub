b06de96011084999a392bb4027a838b5304d3cbd
Austin <108588518+DragonGuyMan@users.noreply.github.com>
2025-12-07 13:18:19 -0500

    Added functions for sending and accepting friend requests

--- services/db/friend.ts
+++ services/db/friend.ts
@@ -4,11 +4,11 @@ import { prisma } from "@/lib/prisma";
  * @param userId The ID of the user who wants to friend another user.
  * @param userToFriendId The ID of the user you want to friend.
  */
-export async function friendUser(userId: string, userToFriendId: string) {
+async function friendUser(userId: string, userToFriendId: string) {
   const alreadyFriend = await isFriend(userId, userToFriendId);
 
   if (!alreadyFriend) {
-    await prisma.friend.create({
+    return await prisma.friend.create({
       data: {
         friendedById: userId,
         friendId: userToFriendId,
@@ -39,4 +39,58 @@ export async function isFriend(userId1: string, userId2: string) {
   });
 
   return followsUser2 !== null && followsUser1 !== null;
+}
+
+/**
+ * Use to retrieve friend requests.
+ */
+export async function getPendingRequests({
+  senderId,
+  receiverId,
+}: {
+  senderId?: string;
+  receiverId?: string;
+}) {
+  return await prisma.friendRequest.findMany({
+    where: { senderId, receiverId },
+  });
+}
+
+/**
+ * Will create a friend request in the database if the two given users aren't
+ * already friends.
+ */
+export async function sendFriendRequest(senderId: string, receiverId: string) {
+  const alreadyFriend = await isFriend(senderId, receiverId);
+
+  if (!alreadyFriend) {
+    return await prisma.friendRequest.create({
+      data: { senderId, receiverId },
+    });
+  }
+}
+
+/**
+ * Will remove an existing friend request and make the two users in question
+ * friends. Only works if there is an existing friend request.
+ */
+export async function acceptFriendRequest(
+  senderId: string,
+  receiverId: string,
+) {
+  const friendRequest = await prisma.friendRequest.findFirst({
+    where: { senderId, receiverId },
+  });
+
+  if (friendRequest) {
+    const friendResult = await friendUser(senderId, receiverId);
+
+    // Delete the friend request once the two users are friends
+    if (friendResult)
+      prisma.friendRequest.delete({
+        where: {
+          senderId_receiverId: { senderId, receiverId },
+        },
+      });
+  }
 }
