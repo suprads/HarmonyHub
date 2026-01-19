307ef3ca04996997dd3c52c8d6d4eeaf0f30ea0b
Austin Schnee <108588518+DragonGuyMan@users.noreply.github.com>
2026-01-19 11:25:54 -0500

    Set Playwright to build app before running tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -56,7 +56,7 @@ export default defineConfig({
 
   // Run your local dev server before starting the tests.
   webServer: {
-    command: "npm run start",
+    command: "npm run build && npm run start",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
