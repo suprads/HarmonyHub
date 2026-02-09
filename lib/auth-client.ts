1960eeedcb415a9ed46e6d810cee81e4c28d62b7
Austin Schnee <108588518+DragonGuyMan@users.noreply.github.com>
2026-02-09 14:04:01 -0500

    Did basic set up for better-auth
    Followed guide at https://www.better-auth.com/docs/installation

---
+++ lib/auth-client.ts
@@ -0,0 +1,9 @@
+import { createAuthClient } from "better-auth/react";
+
+/**
+ * Used to interact with the auth server.
+ */
+export const authClient = createAuthClient({
+  /** The base URL of the server (optional if you're using the same domain) */
+  //baseURL: "http://localhost:3000",
+});
