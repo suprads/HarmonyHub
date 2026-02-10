97b80473668e9171cac01cf7db482c877341b261
Austin Schnee <108588518+DragonGuyMan@users.noreply.github.com>
2026-02-10 12:28:52 -0500

    Added Next cookie handler Better Auth plugin

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,5 @@
 import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 
@@ -10,4 +11,5 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [nextCookies()], // make sure this is the last plugin in the array
 });
