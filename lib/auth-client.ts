945b4692d3ac6904dbd9851091a5e8af7eb5e7f0
Austin Schnee <108588518+DragonGuyMan@users.noreply.github.com>
2026-02-12 16:04:42 -0500

    Added some comments related to Better Auth

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,9 +1,10 @@
 import { createAuthClient } from "better-auth/react";
 
 /**
- * Used to interact with the auth server.
+ * A client instance of Better Auth used to interact with the auth server.
+ * Functions included with this should only be invoked on the client side.
  */
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  //baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
 });
