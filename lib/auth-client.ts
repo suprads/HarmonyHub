fe5666d39f839d584cadeb3bc9dd8a61609cf57e
Austin Schnee <108588518+DragonGuyMan@users.noreply.github.com>
2026-02-23 13:11:07 -0500

    Made options for Better Auth separate variable for better intellisense

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,10 +1,13 @@
+import { BetterAuthClientOptions } from "better-auth";
 import { createAuthClient } from "better-auth/react";
+
+const authClientOptions: BetterAuthClientOptions = {
+  // The base URL of the server (optional if you're using the same domain)
+  // baseURL: "http://localhost:3000",
+};
 
 /**
  * A client instance of Better Auth used to interact with the auth server.
  * Functions included with this should only be invoked on the client side.
  */
-export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  // baseURL: "http://localhost:3000",
-});
+export const authClient = createAuthClient(authClientOptions);
