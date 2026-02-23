d74fbd07825852ea13efe1d06461ed17bef39ed2
Austin Schnee <108588518+DragonGuyMan@users.noreply.github.com>
2026-02-23 13:20:12 -0500

    Added typescript setup for Better Auth client to infer additional field types
    https://www.better-auth.com/docs/concepts/typescript#inferring-additional-fields-on-client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,9 +1,12 @@
 import { BetterAuthClientOptions } from "better-auth";
+import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import type { auth } from "./auth";
 
 const authClientOptions: BetterAuthClientOptions = {
   // The base URL of the server (optional if you're using the same domain)
   // baseURL: "http://localhost:3000",
+  plugins: [inferAdditionalFields<typeof auth>()],
 };
 
 /**
