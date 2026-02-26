cf735a0c6c74ddddf10af0404b2289eb32acf40e
Austin Schnee <108588518+DragonGuyMan@users.noreply.github.com>
2026-02-25 19:44:55 -0500

    Revert "Minor change to Better Auth base URL and DB Migration"
    This reverts commit 412bf65e10b70ae2e9df029b85e478353849e9a9.

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -5,11 +5,7 @@ import type { auth } from "./auth";
 
 const authClientOptions: BetterAuthClientOptions = {
   // The base URL of the server (optional if you're using the same domain)
-  // baseURL: "http://127.0.0.1:3000",
-  baseURL: process.env.BETTER_AUTH_URL,
-  fetchOptions: {
-    credentials: "include",
-  },
+  // baseURL: "http://localhost:3000",
   plugins: [inferAdditionalFields<typeof auth>()],
 };
 
