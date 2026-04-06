53666966eec7ac2cdbcd79d2f8379759fcf946c0
Alioun Sarr Gassama <aliounsarrgassama@gmail.com>
2026-04-06 15:53:06 -0700

    Establish YouTube API Connection

--- next.config.ts
+++ next.config.ts
@@ -39,6 +39,14 @@ const nextConfig: NextConfig = {
       ],
     },
   ],
+  async rewrites() {
+    return [
+      {
+        source: "/api/ytmusic/:path*",
+        destination: "http://127.0.0.1:8000/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
