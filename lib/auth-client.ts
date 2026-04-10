81f0ef2293d20b79091c93bfca04848e68e15bee
Dilpreet Supra <suprads@mail.uc.edu>
2026-04-10 19:22:41 -0400

    Updated auth-client.ts to call .env for browser usage

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -12,6 +12,6 @@ export const authClient = createAuthClient({
   baseURL:
     typeof window !== "undefined"
       ? window.location.origin
-      : process.env.BETTER_AUTH_URL,
+      : process.env.NEXT_PUBLIC_APP_URL,
   plugins: [inferAdditionalFields<typeof auth>()],
 });
