47fd6bdd3df012ff8f59b633790525b87ad20533
Austin Schnee <108588518+DragonGuyMan@users.noreply.github.com>
2026-04-18 14:14:40 -0400

    Updated home page test

--- tests/e2e/home.spec.ts
+++ tests/e2e/home.spec.ts
@@ -3,5 +3,7 @@ import { test, expect } from "@playwright/test";
 test("Home page is successfully navigated to", async ({ page }) => {
   await page.goto("");
 
-  await expect(page.getByRole("heading", { name: /Home/ })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: /Recently Listened Tracks/ }),
+  ).not.toBeVisible();
 });
